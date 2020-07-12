a349316d3e3112ccee12b859f73b2802d24649c0
haphamo <hapham0392@gmail.com>
2020-07-11 21:45:35 -0400

    Created a styled component for the banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import PhoBanner from '../images/test-banner-pho.jpeg';
+import styled from 'styled-components';
+
+const BannerImg = styled.img.attrs({
+  src: PhoBanner,
+  alt: 'banner'
+})`
+  object-fit: none;
+  height: 16em;
+  width: 100%;
+  object-position: 75% 40%;
+  filter: brightness(0.8) opacity(0.6);
+`
 
 const Banner = () => {
-  // TO-DO: Center content container
+  
   return(
     <img src={PhoBanner} alt="banner" style={{objectFit: 'none',
       height: '16em', width: '100%', objectPosition: '75% 40%', filter: 'brightness(0.8) opacity(0.6)'}}></img>
