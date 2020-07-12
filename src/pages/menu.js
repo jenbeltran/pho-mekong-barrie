5b4c0a8858899a52b296a45153a8ed4244c7bf06
haphamo <hapham0392@gmail.com>
2020-07-11 21:54:46 -0400

    Imported Banner component

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,6 +2,8 @@ import React from 'react';
 import SEO from '../components/seo';
 import NavFullMenu from '../components/NavFullMenu/FullMenu.js';
 import MenuSection from '../components/MenuSection/MenuSection.js';
+import Banner from '../components/Banner/Banner.js';
+
 import data from '../components/data';
 
 const MenuPage = () => {
@@ -14,6 +16,7 @@ const MenuPage = () => {
 			<SEO title="Home" />
 			<NavFullMenu />
 			<section className="all-menu-items" style={{ width: '55%' }}>
+				<Banner />
 				{allMenuItems}
 			</section>
 		</div>
