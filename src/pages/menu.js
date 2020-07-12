026e43da5c302c72bad0c8b2120e8481baec7236
haphamo <hapham0392@gmail.com>
2020-07-11 21:58:02 -0400

    Added styling code from prev repo, added to menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -15,8 +15,16 @@ const MenuPage = () => {
 		<div>
 			<SEO title="Home" />
 			<NavFullMenu />
-			<section className="all-menu-items" style={{ width: '55%' }}>
+			<section>
 				<Banner />
+				<div className="pusher" style={{display: 'flex', justifyContent: 'center'}}>
+					<h1  style={{position: 'absolute', top: '4em', transform: 'translateX(3em)'}}>Pho</h1>
+				</div>
+				<section className="description" style={{ textAlign: 'justify', display: 'flex', alignItems: 'center', margin: '1em 2em 0 13em'}}>Pho is a popular Vietnamese noodle soup. Beef bones are simmered for many hours in combination with herbs and spices that help to bring out the flavour. It is served with traditional rice noodles and your choice of meat, or vegetables. Topped with green and white onions and black pepper.
+				</section>
+			
+			</section>
+			<section style={{margin: '0 1em 0 12em'}}>
 				{allMenuItems}
 			</section>
 		</div>
