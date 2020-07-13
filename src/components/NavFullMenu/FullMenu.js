19d371fe716c716541cdb44423ee76c92d3f72b9
haphamo <hapham0392@gmail.com>
2020-07-13 10:36:15 -0400

    Tested the active state of the side nav menu. Added name and active attr in Menu.Item and created state and a function to handle the active state

--- src/components/NavFullMenu/FullMenu.js
+++ src/components/NavFullMenu/FullMenu.js
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'gatsby';
 import { Menu, Sidebar } from 'semantic-ui-react';
 
-const SidebarMenuVisible = () => (
-	<Sidebar as={Menu} animation="push" icon="labeled" inverted vertical visible>
-		<Menu.Item as="a">
-			<Link href="/">Pho Mekong</Link>
-		</Menu.Item>
-		<Menu.Item as="a">
-			<Link href="/menu">Popular Items</Link>
-		</Menu.Item>
-		<Menu.Item as="a">
-			<Link href="/menu">Full Menu</Link>
-		</Menu.Item>
-		<Menu.Item as="a">Pho</Menu.Item>
-		<Menu.Item as="a">Pad Thai</Menu.Item>
-		<Menu.Item as="a">Vermicelli</Menu.Item>
-		<Menu.Item as="a">Appetizers</Menu.Item>
-		<Menu.Item as="a">Steamed Rice Dishes</Menu.Item>
-		<Menu.Item as="a">Fried Rice Dishes</Menu.Item>
-		<Menu.Item as="a">Noodle Soups</Menu.Item>
-		<Menu.Item as="a">Small Soups</Menu.Item>
-		<Menu.Item as="a">Stir-fry Noodles</Menu.Item>
-		<Menu.Item as="a">Signature Dishes</Menu.Item>
-		<Menu.Item as="a">Congee</Menu.Item>
-		<Menu.Item as="a">Curry</Menu.Item>
-		<Menu.Item as="a">Salads</Menu.Item>
-		<Menu.Item as="a">Beverages</Menu.Item>
-		<Menu.Item as="a">
-			<Link href="/">Contact Us</Link>
-		</Menu.Item>
+const SidebarMenuVisible = () => {
+
+	const [activeItem, setActiveItem] = useState('');
+
+	const handleItemClick = (evt, {name}) => {
+		setActiveItem(name);
+		console.log('it works')
+	};
+
+	return (
+		<Sidebar as={Menu} animation="push" icon="labeled" inverted vertical visible>
+			<Menu.Item as="a">
+				<Link href="/">Pho Mekong</Link>
+			</Menu.Item>
+			<Menu.Item as="a">
+				<Link href="/menu">Popular Items</Link>
+			</Menu.Item>
+			<Menu.Item as="a">
+				<Link href="/menu">Full Menu</Link>
+			</Menu.Item>
+			<Menu.Item as="a" name="pho" active={activeItem === 'pho'} onClick={handleItemClick}>Pho</Menu.Item>
+			<Menu.Item as="a" name="padthai" active={activeItem === 'padthai'} onClick={handleItemClick}>Pad Thai</Menu.Item>
+			<Menu.Item as="a" name="">Vermicelli</Menu.Item>
+			<Menu.Item as="a" name="">Appetizers</Menu.Item>
+			<Menu.Item as="a" name="">Steamed Rice Dishes</Menu.Item>
+			<Menu.Item as="a" name="">Fried Rice Dishes</Menu.Item>
+			<Menu.Item as="a" name="">Noodle Soups</Menu.Item>
+			<Menu.Item as="a" name="">Small Soups</Menu.Item>
+			<Menu.Item as="a" name="">Stir-fry Noodles</Menu.Item>
+			<Menu.Item as="a" name="">Signature Dishes</Menu.Item>
+			<Menu.Item as="a" name="">Congee</Menu.Item>
+			<Menu.Item as="a" name="">Curry</Menu.Item>
+			<Menu.Item as="a" name="">Salads</Menu.Item>
+			<Menu.Item as="a" name="">Beverages</Menu.Item>
+			<Menu.Item as="a">
+				<Link href="/">Contact Us</Link>
+			</Menu.Item>
 	</Sidebar>
-);
+	)
+	
+};
 
 export default SidebarMenuVisible;
