97be411867ede4f10b51bf88b3851ad434f913dc
haphamo <hapham0392@gmail.com>
2020-07-13 10:44:12 -0400

    removed the Link from FullMenu and made it the default active state. Added name, active state and onClick attr for the rest of the nav items

--- src/components/NavFullMenu/FullMenu.js
+++ src/components/NavFullMenu/FullMenu.js
@@ -4,7 +4,7 @@ import { Menu, Sidebar } from 'semantic-ui-react';
 
 const SidebarMenuVisible = () => {
 
-	const [activeItem, setActiveItem] = useState('');
+	const [activeItem, setActiveItem] = useState('fullMenu');
 
 	const handleItemClick = (evt, {name}) => {
 		setActiveItem(name);
@@ -19,23 +19,21 @@ const SidebarMenuVisible = () => {
 			<Menu.Item as="a">
 				<Link href="/menu">Popular Items</Link>
 			</Menu.Item>
-			<Menu.Item as="a">
-				<Link href="/menu">Full Menu</Link>
-			</Menu.Item>
+			<Menu.Item as="a" name="fullMenu" active={activeItem === 'fullMenu'} onClick={handleItemClick}>Full Menu</Menu.Item>
 			<Menu.Item as="a" name="pho" active={activeItem === 'pho'} onClick={handleItemClick}>Pho</Menu.Item>
 			<Menu.Item as="a" name="padthai" active={activeItem === 'padthai'} onClick={handleItemClick}>Pad Thai</Menu.Item>
-			<Menu.Item as="a" name="">Vermicelli</Menu.Item>
-			<Menu.Item as="a" name="">Appetizers</Menu.Item>
-			<Menu.Item as="a" name="">Steamed Rice Dishes</Menu.Item>
-			<Menu.Item as="a" name="">Fried Rice Dishes</Menu.Item>
-			<Menu.Item as="a" name="">Noodle Soups</Menu.Item>
-			<Menu.Item as="a" name="">Small Soups</Menu.Item>
-			<Menu.Item as="a" name="">Stir-fry Noodles</Menu.Item>
-			<Menu.Item as="a" name="">Signature Dishes</Menu.Item>
-			<Menu.Item as="a" name="">Congee</Menu.Item>
-			<Menu.Item as="a" name="">Curry</Menu.Item>
-			<Menu.Item as="a" name="">Salads</Menu.Item>
-			<Menu.Item as="a" name="">Beverages</Menu.Item>
+			<Menu.Item as="a" name="vermicelli" active={activeItem === 'vermicelli'} onClick={handleItemClick}>Vermicelli</Menu.Item>
+			<Menu.Item as="a" name="appetizers" active={activeItem === 'appetizers'} onClick={handleItemClick}>Appetizers</Menu.Item>
+			<Menu.Item as="a" name="rice" active={activeItem === 'rice'} onClick={handleItemClick}>Steamed Rice Dishes</Menu.Item>
+			<Menu.Item as="a" name="friedRice" active={activeItem === 'friedRice'} onClick={handleItemClick}>Fried Rice Dishes</Menu.Item>
+			<Menu.Item as="a" name="noodleSoups" active={activeItem === 'noodleSoups'} onClick={handleItemClick}>Noodle Soups</Menu.Item>
+			<Menu.Item as="a" name="smallSoups" active={activeItem === 'smallSoups'} onClick={handleItemClick}>Small Soups</Menu.Item>
+			<Menu.Item as="a" name="stirFryNoodles" active={activeItem === 'stirFryNoodles'} onClick={handleItemClick}>Stir-fry Noodles</Menu.Item>
+			<Menu.Item as="a" name="sig" active={activeItem === 'sig'} onClick={handleItemClick}>Signature Dishes</Menu.Item>
+			<Menu.Item as="a" name="congee" active={activeItem === 'congee'} onClick={handleItemClick}>Congee</Menu.Item>
+			<Menu.Item as="a" name="curry" active={activeItem === 'curry'} onClick={handleItemClick}>Curry</Menu.Item>
+			<Menu.Item as="a" name="salads" active={activeItem === 'salads'} onClick={handleItemClick}>Salads</Menu.Item>
+			<Menu.Item as="a" name="beverages" active={activeItem === 'beverages'} onClick={handleItemClick}>Beverages</Menu.Item>
 			<Menu.Item as="a">
 				<Link href="/">Contact Us</Link>
 			</Menu.Item>
