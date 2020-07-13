4191062832c582529c12638491960b65f2f5cbf5
jenbeltran <francescajenbeltran@gmail.com>
2020-07-13 10:25:58 -0400

    add landing page for mobile

--- src/components/LandingPage/Landing.css.js
+++ src/components/LandingPage/Landing.css.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 const LandingSection = styled.section`
-	position: relative;
-	display: flex;
-	margin: auto;
-	padding: 5rem 12rem 5rem 12rem;
+	@media (min-width: 481px) {
+		position: relative;
+		display: flex;
+		margin: auto;
+		padding: 5rem 5rem 5rem 20rem;
+	}
 
 	@media (max-width: 480px) {
 		margin: auto;
@@ -13,18 +15,23 @@ const LandingSection = styled.section`
 `;
 
 const Wrapper = styled.div`
-	padding: 5rem 12rem 5rem 5rem;
-	border-left: 5px solid grey;
+	@media (min-width: 481px) {
+		padding: 5rem 12rem 5rem 5rem;
+		border-left: 5px solid grey;
+	}
+
 	@media (max-width: 480px) {
-		padding: 2rem 2rem 6rem 2rem;
+		padding: 5rem;
 	}
 `;
 
 const Wrapper2 = styled.div`padding-top: 4rem;`;
 
 const Headline = styled.h2`
-	font-size: 2rem;
-	font-weight: 500;
+	@media (min-width: 481px) {
+		font-size: 2rem;
+		font-weight: 500;
+	}
 
 	@media (max-width: 480px) {
 		font-size: 2rem;
@@ -35,7 +42,9 @@ const Headline = styled.h2`
 const Info = styled.p`font-size: 1.3rem;`;
 
 const RestaurantPhoto = styled.img`
-	height: 35rem;
+	@media (min-width: 481px) {
+		height: 35rem;
+	}
 	@media (max-width: 480px) {
 		width: 100%;
 		height: auto;
---
+++ src/components/NavFullMenu/FullMenu.css.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const MenuSection = styled.section`@media screen and (max-width: 780px) {margin: auto;}`;
+
+export { MenuSection };
