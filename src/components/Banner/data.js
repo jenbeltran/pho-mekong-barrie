1d24b0036da90dcb59305ea8a9d954c10bdd329b
haphamo <hapham0392@gmail.com>
2020-07-14 17:20:01 -0400

    Added the images into BannerData

--- src/components/Banner/data.js
+++ src/components/Banner/data.js
@@ -3,6 +3,16 @@ import Pho from '../../images/test-banner-pho.jpeg';
 import Appetizer from '../../images/appetizer.png';
 import Congee from '../../images/congee.png';
 import NoodleSoup from '../../images/noodle-soup.png';
+import PadThai from '../../images/pad-thai.jpg';
+import Beverages from '../../images/beverages.jpg';
+import Curry from '../../images/curry.jpg';
+import Salad from '../../images/salad.jpg';
+import FriedRice from '../../images/fried-rice.png';
+import SmallSoup from '../../images/small-soup.png';
+import SteamedRice from '../../images/steamed-rice.png';
+import StirFry from '../../images/stir-fry-noodle.png';
+import Vermicelli from '../../images/vermicelli.png';
+import { Fragment } from 'react';
 
 const BannerData = {
   'Full Menu': {
@@ -23,12 +33,12 @@ const BannerData = {
   'Pad Thai': {
     title: 'Pad Thai',
     desc: 'Lorem',
-    img: ''
+    img: PadThai
   },
   'Vermicelli': {
     title: 'Vermicelli',
     desc: 'Lorem',
-    img: ''
+    img: Vermicelli
   },
   'Appetizers': {
     title: 'Appetizers',
@@ -38,12 +48,12 @@ const BannerData = {
   'Steamed Rice Dishes': {
     title: 'Steamed Rice Dishes',
     desc: 'Lorem',
-    img: ''
+    img: SteamedRice
   },
   'Fried Rice Dishes': {
     title: 'Fried Rice Dishes',
     desc: 'Lorem',
-    img: ''
+    img: FriedRice
   },
   'Noodle Soups': {
     title: 'Noodle Soups',
@@ -53,12 +63,12 @@ const BannerData = {
   'Small Soups': {
     title: 'Small Soups',
     desc: 'Lorem',
-    img: ''
+    img: SmallSoup
   },
   'Stir-fry Noodles': {
     title: 'Stir-fry Noodles',
     desc: 'Lorem',
-    img: ''
+    img: StirFry
   },
   'Signature Dishes': {
     title: 'Signature Dishes',
@@ -73,17 +83,17 @@ const BannerData = {
   'Curry': {
     title: 'Curry',
     desc: 'Lorem',
-    img: ''
+    img: Curry
   },
   'Salads': {
     title: 'Salads',
     desc: 'Lorem',
-    img: ''
+    img: Salad
   },
   'Beverages': {
     title: 'Beverages',
     desc: 'Lorem',
-    img: ''
+    img: Beverages
   },
   'Contact Us': {
     title: 'Contact Us',
