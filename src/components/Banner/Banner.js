df14702339ae394cbd60c845be6d95ba7f133eb9
haphamo <hapham0392@gmail.com>
2020-07-14 16:50:10 -0400

    Removed Desc styled component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { BannerHeader, BannerImg, BannerSection, Desc } from './Banner.css.js'
+import { BannerHeader, BannerImg, BannerSection } from './Banner.css.js'
+import BannerData from './data.js';
 
 // TODO: Pass the src as props to styled component, it is currently hardcoded
 // Currently the BannerHeader and Desc are hardcoded, make this take props to make it reusable
 
 // This commponent is the top section of the content
 const Banner = ({activeItem}) => {
-  
+  const currentImg = BannerData[activeItem][img];
+  console.log(currentImg)
   return(
     <section>
-      <BannerImg />
+      <BannerImg currentImg={currentImg}/>
       <BannerSection>
-        <BannerHeader>Pho</BannerHeader>
+      <BannerHeader>{activeItem}</BannerHeader>
       </BannerSection>
-      <Desc>
-				Pho is a popular Vietnamese noodle soup. Beef bones are simmered for many hours in combination with herbs and spices that help to bring out the flavour. It is served with traditional rice noodles and your choice of meat, or vegetables. Topped with green and white onions and black pepper.
-				</Desc>
-
     </section>
   )
 }
