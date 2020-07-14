bfb59cbbc415910311249bcd981b1820902aa000
haphamo <hapham0392@gmail.com>
2020-07-14 16:34:19 -0400

    removed Header p since it's in the Banner now. Addded min-height for the Paragraph in styled component

--- src/components/MenuSection/MenuSection.css.js
+++ src/components/MenuSection/MenuSection.css.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-const Header = styled.h4`
-	padding: 1em 1em 0em 1em
-`
+// const Header = styled.h4`
+// 	padding: 1em 1em 0em 1em;
+// `
 const Paragraph = styled.p`
-	padding: 0em 1.5em
+	padding: 1em 1.5em;
+	margin-bottom: 0;
+	min-height: 6em;
 `
-export { Header, Paragraph };
+export { Paragraph };
