540892cbd2d6d2420ee9d103ec3854fdc8cab8c0
haphamo <hapham0392@gmail.com>
2020-07-14 14:46:42 -0400

    Refactored the NavDesktop

--- src/components/NavDesktop/NavDesktop.js
+++ src/components/NavDesktop/NavDesktop.js
@@ -1,41 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'gatsby';
 import { Menu, Sidebar } from 'semantic-ui-react';
 import { DesktopNavSection } from './NavDesktop.css';
+import NavData from './data.js';
 
 const SidebarMenuVisible = ({activeItem, setActiveItem}) => {
 
 	const handleItemClick = (evt, {name}) => {
-
 		setActiveItem(name);
 	};
 
+	// maps through the NavData to render the full Nav Bar
+	// TODO: Check if there is a way to only add the onClick to the parent Sidebar, so all children components get the handler as well
+	const getAllNavItems = NavData.map(nav => {
+		const { name, link } = nav;
+		return(
+			<Menu.Item as="a" name={name} active={activeItem === `${name}`} onClick={handleItemClick}>
+				{link ? <Link href={`${link}`}>{name}</Link> : <Fragment>{name}</Fragment>}
+			</Menu.Item>
+		)
+	});
+
 	return (
 		<DesktopNavSection>
-
 				<Sidebar as={Menu} animation="push" icon="labeled" inverted vertical visible>
-					<Menu.Item as="a" name="Pho Mekong" active={activeItem === 'Pho Mekong'} onClick={handleItemClick}>
-						<Link href="/">Pho Mekong</Link>
-					</Menu.Item>
-					<Menu.Item as="a" name="Popular Items" active={activeItem === 'Popular Items'} onClick={handleItemClick}>Popular Items</Menu.Item>
-					<Menu.Item as="a" name="Full Menu" active={activeItem === 'Full Menu'} onClick={handleItemClick}>Full Menu</Menu.Item>
-					<Menu.Item as="a" name="Pho" active={activeItem === 'Pho'} onClick={handleItemClick}>Pho</Menu.Item>
-					<Menu.Item as="a" name="Pad Thai" active={activeItem === 'Pad Thai'} onClick={handleItemClick}>Pad Thai</Menu.Item>
-					<Menu.Item as="a" name="Vermicelli" active={activeItem === 'Vermicelli'} onClick={handleItemClick}>Vermicelli</Menu.Item>
-					<Menu.Item as="a" name="Appetizers" active={activeItem === 'Appetizers'} onClick={handleItemClick}>Appetizers</Menu.Item>
-					<Menu.Item as="a" name="Steamed Rice Dishes" active={activeItem === 'Steamed Rice Dishes'} onClick={handleItemClick}>Steamed Rice Dishes</Menu.Item>
-					<Menu.Item as="a" name="Fried Rice Dishes" active={activeItem === 'Fried Rice Dishes'} onClick={handleItemClick}>Fried Rice Dishes</Menu.Item>
-					<Menu.Item as="a" name="Noodle Soups" active={activeItem === 'Noodle Soups'} onClick={handleItemClick}>Noodle Soups</Menu.Item>
-					<Menu.Item as="a" name="Small Soups" active={activeItem === 'Small Soups'} onClick={handleItemClick}>Small Soups</Menu.Item>
-					<Menu.Item as="a" name="Stir-fry Noodles" active={activeItem === 'Stir-fry Noodles'} onClick={handleItemClick}>Stir-fry Noodles</Menu.Item>
-					<Menu.Item as="a" name="Signature Dishes" active={activeItem === 'Signature Dishes'} onClick={handleItemClick}>Signature Dishes</Menu.Item>
-					<Menu.Item as="a" name="Congee" active={activeItem === 'Congee'} onClick={handleItemClick}>Congee</Menu.Item>
-					<Menu.Item as="a" name="Curry" active={activeItem === 'Curry'} onClick={handleItemClick}>Curry</Menu.Item>
-					<Menu.Item as="a" name="Salads" active={activeItem === 'Salads'} onClick={handleItemClick}>Salads</Menu.Item>
-					<Menu.Item as="a" name="Beverages" active={activeItem === 'Beverages'} onClick={handleItemClick}>Beverages</Menu.Item>
-					<Menu.Item as="a">
-						<Link href="/">Contact Us</Link>
-					</Menu.Item>
+					{getAllNavItems}
 			</Sidebar>
 		</DesktopNavSection>
 	)
