330347654207fd1852f6a8928646287b7a28e841
haphamo <hapham0392@gmail.com>
2020-07-14 15:18:51 -0400

    Completed structure for data, just need to import the img src

---
+++ src/components/Banner/data.js
@@ -0,0 +1,91 @@
+// IMPORT IMGS HERE TO USE
+
+const BannerData = {
+  'Full Menu': {
+    title: 'Full Menu',
+    desc: 'Lorem Ipsum',
+    img: 'img'
+  },
+  'Popular Items': {
+    title: 'Popular Items',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Pho': {
+    title: 'Pho',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Pad Thai': {
+    title: 'Pad Thai',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Vermicelli': {
+    title: 'Vermicelli',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Appetizers': {
+    title: 'Appetizers',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Steamed Rice Dishes': {
+    title: 'Steamed Rice Dishes',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Fried Rice Dishes': {
+    title: 'Fried Rice Dishes',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Noodle Soups': {
+    title: 'Noodle Soups',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Small Soups': {
+    title: 'Small Soups',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Stir-fry Noodles': {
+    title: 'Stir-fry Noodles',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Signature Dishes': {
+    title: 'Signature Dishes',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Congee': {
+    title: 'Congee',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Curry': {
+    title: 'Curry',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Salads': {
+    title: 'Salads',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Beverages': {
+    title: 'Beverages',
+    desc: 'Lorem',
+    img: 'img'
+  },
+  'Contact Us': {
+    title: 'Contact Us',
+    desc: 'Lorem',
+    img: 'img'
+  },
+};
+
+export default BannerData;
