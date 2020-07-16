1d3def8e1f56a2b477379cee9087fe7f57bd1942
haphamo <hapham0392@gmail.com>
2020-07-16 14:18:35 -0400

    Added keyboard accessiblity on Nav

--- src/components/NavDesktop/NavDesktop.js
+++ src/components/NavDesktop/NavDesktop.js
@@ -8,20 +8,22 @@ import { uuid } from 'uuidv4';
 const SidebarMenuVisible = ({activeItem, setActiveItem}) => {
 
 	const handleItemClick = (evt, {name}) => {
-		
 		setActiveItem(name);
 	};
 
 	const handleItemTab = (evt) => {
-		console.log(evt.target.dataset.name)
-		// setActiveItem(evt.target.dataset.name)
+		// console.log(evt.target.dataset.name)
+		// console.log(evt.nativeEvent.key)
+		if(evt.nativeEvent.key === 'Enter') {
+			setActiveItem(evt.target.dataset.name)
+		}
 	}
 
 	// TODO: Check if there is a way to only add the onClick to the parent Sidebar, so all children components get the handler as well
 	const getAllNavItems = NavData.map(nav => {
 		const { name } = nav;
 		return(
-			<Menu.Item key={uuid()} name={name} data-name={name} active={activeItem === `${name}`} tabIndex={0} onKeyDown={handleItemTab} onClick={handleItemClick}>
+			<Menu.Item key={uuid()}	name={name} data-name={name} type="button" active={activeItem === `${name}`} tabIndex={0} onKeyDown={handleItemTab} onClick={handleItemClick}>
 				{name}
 			</Menu.Item>
 		)
