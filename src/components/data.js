eb2076fa2581d748c7d1fa23f091d500ae70b3fc
haphamo <hapham0392@gmail.com>
2020-07-17 13:18:24 -0400

    Removed Fragment import, changed one of the img names from H4 to NooodleSoup2

--- src/components/data.js
+++ src/components/data.js
@@ -211,6 +211,30 @@ const data = [
 			{ code: 'S8A', name: 'Stir fry ginger shrimp with steamed rice', price: 1395 },
 			{ code: 'S8B', name: 'Spicy satsy stir fry ginger shrimp with steamed rice', price: 1395 }
 		]
+	},
+	{
+		header: 'Popular Items',
+		list: [
+			{ code: 'A1', name: 'Crispy Spring Rolls', price: 495 },
+			{
+				code: 'A5',
+				name: 'Fresh Salad Rolls',
+				price: 495,
+				desc:
+					'Rice paper rolls served with fine vermicelli noodles, lettuce, cucumber, and served with sweet home-made sauce. Rolled with one of the options below. If you prefer a fish sauce as dip, let us know.',
+				subOptions: [ 'A. Shrimp & Pork', 'B. Grilled Beef', 'C. Grilled Chicken', 'D. Vegetables' ]
+			},
+			{ code: 'P1', name: 'Rare beef', subOptions: [ 'M: 9.95', 'L: 10.95', 'XL: 11.95' ] },
+			{
+				code: 'P10',
+				name: 'House Special',
+				desc: '(Assorted beef, with vegetables and shrimp)',
+				subOptions: [ 'M: 10.95', 'L: 12.95', 'XL: 12.95' ]
+			},
+			{ code: 'V2C', name: 'Vermicelli with spring roll and grilled chicken', price: 1195 },
+			{ code: 'T2', name: 'Chicken Pad Thai', price: 1195 },
+			{ code: 'T6', name: 'Chicken Tom Yum Noodle Soup', price: 1195 },
+		]
 	}
 ];
 
