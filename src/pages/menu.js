a0cca639bd72e6819c9ff140256cf1b6b61808db
haphamo <hapham0392@gmail.com>
2020-07-19 15:56:49 -0400

    Removed logic to filter data being passed, it is now coming from the props from createPages. Removed Fragment, as it's not needed anymore and the logic for rendering the LandingPage component

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import SEO from '../components/seo';
 import NavDesktop from '../components/NavDesktop/NavDesktop.js';
 import MenuSection from '../components/MenuSection/MenuSection.js';
@@ -12,32 +12,18 @@ import { uuid } from 'uuidv4';
 
 const MenuContent = styled.section`margin: 0em 1em 0 13em;`;
 
-const MenuPage = () => {
+const MenuPage = (props) => {
+	const passedPageContent = [{...props.pageContext.category}];
 	const [ activeItem, setActiveItem ] = useState('Pho Mekong');
 
-	// Logic which updates menu content depending on the state of the Navmenu
-	let fitlered;
-
-	if (activeItem === 'Full Menu') {
-		fitlered = data;
-	} else {
-		fitlered = data.filter((section) => section.header === activeItem);
-	}
-
-	const MenuItems = fitlered.map((section) => <MenuSection key={uuid()} activeItem={activeItem} section={section} />);
+	const MenuItems = passedPageContent.map((section) => <MenuSection key={uuid()} activeItem={activeItem} section={section} />);
 
 	return (
 		<section>
 			<SEO title="Home" />
 			<NavDesktop activeItem={activeItem} setActiveItem={setActiveItem} />
-			{activeItem === 'Pho Mekong' ? (
-				<Landing />
-			) : (
-				<Fragment>
-					<Banner activeItem={activeItem}/>
-					<MenuContent>{MenuItems}</MenuContent>
-				</Fragment>
-			)}
+				<Banner activeItem={passedPageContent[0].header}/>
+				<MenuContent>{MenuItems}</MenuContent>
 		</section>
 	);
 };
