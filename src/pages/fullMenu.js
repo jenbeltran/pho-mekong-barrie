393bf170be49c78e3b2a8fd7bd233f1cd27f2bcf
haphamo <hapham0392@gmail.com>
2020-07-21 10:08:43 -0400

    Removed console log, this page is only rendered for the full menu

--- src/pages/fullMenu.js
+++ src/pages/fullMenu.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import SEO from '../components/seo';
 import NavDesktop from '../components/NavDesktop/NavDesktop.js';
+import NavMobile from '../components/NavMobile/NavMobile.js';
 import MenuSection from '../components/MenuSection/MenuSection.js';
 import Banner from '../components/Banner/Banner.js';
 import styled from 'styled-components';
@@ -10,7 +11,7 @@ import data from '../components/data.js'
 const MenuContent = styled.section`margin: 0em 1em 0 13em;`;
 
 const FullMenu = (props) => {
-	console.log(props.pageContext)
+	
 	const [ activeItem, setActiveItem ] = useState('Full Menu');
 
 	const MenuItems = data.map((section) => <MenuSection key={uuid()} activeItem={activeItem} section={section} />);
@@ -18,6 +19,7 @@ const FullMenu = (props) => {
 	return (
 		<section>
 			<SEO title="Home" />
+      <NavMobile />
 			<NavDesktop activeItem={activeItem} setActiveItem={setActiveItem} />
 				<Banner activeItem="Full Menu"/>
 				<MenuContent>{MenuItems}</MenuContent>
