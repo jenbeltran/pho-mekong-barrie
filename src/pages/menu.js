2255b69c728af8bc85c6274b25883c9f49137db1
haphamo <hapham0392@gmail.com>
2020-07-21 10:10:40 -0400

    Returned code to what it previously was with the menu sections rendering properly, changed the intial state and props to take the props passed from pageContext

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import SEO from '../components/seo';
 import NavDesktop from '../components/NavDesktop/NavDesktop.js';
+import NavMobile from '../components/NavMobile/NavMobile.js';
 import MenuSection from '../components/MenuSection/MenuSection.js';
 import Banner from '../components/Banner/Banner.js';
 import styled from 'styled-components';
@@ -20,6 +21,7 @@ const MenuPage = (props) => {
 	return (
 		<section>
 			<SEO title="Home" />
+			<NavMobile/>
 			<NavDesktop activeItem={activeItem} setActiveItem={setActiveItem} />
 				<Banner activeItem={passedPageContent[0].header}/>
 				<MenuContent>{MenuItems}</MenuContent>
