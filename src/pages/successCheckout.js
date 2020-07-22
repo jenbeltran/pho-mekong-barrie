6f7395e811278e9ad6666d19f79536dff1501e36
jenbeltran <francescajenbeltran@gmail.com>
2020-07-22 14:35:20 -0400

    add checkout success page

--- src/pages/successCheckout.js
+++ src/pages/successCheckout.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import SEO from '../components/seo';
 import NavDesktop from '../components/NavDesktop/NavDesktop.js';
-import NavMobile from '../components/NavMobile/NavMobile.js';
+import NavMobile from '../components/NavMobile/NavMobile';
+import { RestaurantData } from '../components/LandingPage/data';
+import { LandingSection, Wrapper, Wrapper2, Headline, Info, LogoPhoto } from '../components/LandingPage/Landing.css';
+import logo from '../images/logo-favicon.png';
 
-const SuccessCheckout = () => {
+const SuccessCheckout = (props) => {
+	const { headline, street, postalcode, phone } = RestaurantData;
 	return (
 		<div>
-			<SEO title="SuccessCheckout" />
 			<NavDesktop />
 			<NavMobile />
-			<h1>Thank you for your order - an email has been sent with a receipt</h1>
+
+			<LandingSection>
+				<Wrapper>
+					<Headline>Thank you for ordering!</Headline>
+					<Info>
+						A receipt has been sent to the email address entered. Please provide receipt number upon pickup.
+					</Info>
+					<Wrapper2>
+						<Headline>Pickup Address</Headline>
+						<Info>{headline}</Info>
+						<Info>{street}</Info>
+						<Info>{postalcode}</Info>
+					</Wrapper2>
+					<Wrapper2>
+						<Info>
+							If you do not receive an email within the next 10 minutes, please contact us at {phone}
+						</Info>
+						<LogoPhoto src={logo} />
+					</Wrapper2>
+				</Wrapper>
+			</LandingSection>
 		</div>
 	);
 };
--- src/pages/viewCart.js
+++ src/pages/viewCart.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import SEO from '../components/seo';
 import NavDesktop from '../components/NavDesktop/NavDesktop.js';
-import NavMobile from '../components/NavMobile/NavMobile.js';
+import MenuSection from '../components/MenuSection/MenuSection.js';
+import Banner from '../components/Banner/Banner.js';
+import { uuid } from 'uuidv4';
+import data from '../components/data.js';
 import Checkout from '../components/Checkout/Checkout';
 
-const ViewCart = () => {
+const MenuContent = styled.section`margin: 0em 1em 0 13em;`;
+
+const ViewCart = (props) => {
+	console.log(props.pageContext);
+	const [ activeItem, setActiveItem ] = useState('Full Menu');
+
+	const MenuItems = data.map((section) => <MenuSection key={uuid()} activeItem={activeItem} section={section} />);
+
 	return (
-		<div>
-			<SEO title="shoppingCart" />
+		<section>
+			<SEO title="Home" />
 			<NavDesktop />
-			<NavMobile />
-			<h1>Shopping Cart</h1>
+			<Banner activeItem="Full Menu" />
+			<MenuContent>{MenuItems}</MenuContent>
 			<Checkout />
-		</div>
+		</section>
 	);
 };
-
 export default ViewCart;
