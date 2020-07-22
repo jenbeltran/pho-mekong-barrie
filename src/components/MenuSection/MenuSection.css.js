13914d5a181706fa430101a178d103d999a197c0
haphamo <hapham0392@gmail.com>
2020-07-21 23:52:20 -0400

    Added id selector in styled components for global styles

--- src/components/MenuSection/MenuSection.css.js
+++ src/components/MenuSection/MenuSection.css.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 
-const Paragraph = styled.p`
+const Paragraph = styled.p.attrs({
+	id: 'section-description'
+})`
 	padding: 1rem 1.25em 0em 1.25em;
 	margin-bottom: 0;
 	min-height: 3em;
 	text-align: justify;
 	display: flex;
 	align-items: center;
+	line-height: 1.9em;
 `
 
 const FullMenuSubHeader = styled.h1`
