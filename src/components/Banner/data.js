b503f8e09d7bfeedd5897d5e806029afac4bab2d
haphamo <hapham0392@gmail.com>
2020-07-28 14:03:19 -0400

    Removed lengthy import statements for each image and replaced with one coming from the images.js file

--- src/components/Banner/data.js
+++ src/components/Banner/data.js
@@ -1,19 +1,4 @@
-// TODO: Make sure each category has a unique banner img
-import Pho from '../../images/test-banner-pho.jpeg';
-import Appetizer from '../../images/appetizer.png';
-import Congee from '../../images/congee.png';
-import NoodleSoup from '../../images/noodle-soup.png';
-import PadThai from '../../images/pad-thai.jpg';
-import Beverages from '../../images/beverages.jpg';
-import Curry from '../../images/curry.jpg';
-import Salad from '../../images/salad.jpg'; // 
-import FriedRice from '../../images/fried-rice.png';
-import SmallSoup from '../../images/small-soup.png';
-import SteamedRice from '../../images/steamed-rice.png';
-import StirFry from '../../images/stir-fry-noodle.png';
-import Vermicelli from '../../images/vermicelli.png';
-import Signature from '../../images/sig.jpeg';
-import NoodleSoup2 from '../../images/H4.jpeg';
+import { Pho, Appetizer, Congee, PadThai, Beverages, Curry, Salad, FriedRice, SmallSoup, SteamedRice, StirFry, Vermicelli, Signature, NoodleSoup2, BunBoHue, TomYumNoodle } from '../../images/images.js';
 
 const BannerData = {
   'Full Menu': {
@@ -40,6 +25,11 @@ const BannerData = {
     title: 'Vermicelli',
     desc: 'Lorem',
     img: Vermicelli
+  },
+  'Spicy Tom Yum Noodle Soup': {
+    title: 'Tom Yum Noodle Soup',
+    desc: 'Lorem',
+    img: TomYumNoodle,
   },
   'Appetizers': {
     title: 'Appetizers',
