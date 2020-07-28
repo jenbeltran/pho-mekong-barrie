f33c694ad1737de13f6894326104bc2195b762ab
haphamo <hapham0392@gmail.com>
2020-07-28 15:01:58 -0400

    Removed desc from data obj obj

--- src/components/Banner/data.js
+++ src/components/Banner/data.js
@@ -3,92 +3,74 @@ import { Pho, Appetizer, Congee, PadThai, Beverages, Curry, Salad, FriedRice, Sm
 const BannerData = {
   'Full Menu': {
     title: 'Full Menu',
-    desc: 'Lorem Ipsum',
     img: ''
   },
   'Popular Items': {
     title: 'Popular Items',
-    desc: 'Lorem',
     img: ''
   },
   'Pho': {
     title: 'Pho',
-    desc: 'Lorem',
     img: Pho
   },
   'Pad Thai': {
     title: 'Pad Thai',
-    desc: 'Lorem',
     img: PadThai
   },
   'Vermicelli': {
     title: 'Vermicelli',
-    desc: 'Lorem',
     img: Vermicelli
   },
-  'Spicy Tom Yum Noodle Soup': {
+  'Tom Yum Noodle Soup': {
     title: 'Tom Yum Noodle Soup',
-    desc: 'Lorem',
     img: TomYumNoodle,
   },
   'Appetizers': {
     title: 'Appetizers',
-    desc: 'Lorem',
     img: Appetizer
   },
   'Steamed Rice Dishes': {
     title: 'Steamed Rice Dishes',
-    desc: 'Lorem',
     img: SteamedRice
   },
   'Fried Rice Dishes': {
     title: 'Fried Rice Dishes',
-    desc: 'Lorem',
     img: FriedRice
   },
   'Noodle Soups': {
     title: 'Noodle Soups',
-    desc: 'Lorem',
     img: NoodleSoup2,
   },
   'Small Soups': {
     title: 'Small Soups',
-    desc: 'Lorem',
     img: SmallSoup
   },
   'Stir-fry Noodles': {
     title: 'Stir-fry Noodles',
-    desc: 'Lorem',
     img: StirFry
   },
   'Signature Dishes': {
     title: 'Signature Dishes',
-    desc: 'Lorem',
     img: Signature
   },
   'Congee': {
     title: 'Congee',
-    desc: 'Lorem',
     img: Congee,
   },
   'Curry': {
     title: 'Curry',
-    desc: 'Lorem',
     img: Curry
   },
   'Salads': {
     title: 'Salads',
-    desc: 'Lorem',
     img: Salad
   },
   'Beverages': {
     title: 'Beverages',
-    desc: 'Lorem',
     img: Beverages
   },
   'Contact Us': {
     title: 'Contact Us',
-    desc: 'Lorem',
     img: ''
   },
 };
