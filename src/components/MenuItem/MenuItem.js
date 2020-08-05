428b02b845e392bce275873638c744175b484026
haphamo <hapham0392@gmail.com>
2020-08-05 14:18:37 -0400

    Added cart icon from semantic iu, to display on the right of the price for menu items (except pho)

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,44 +1,57 @@
-import React from 'react';
-import { ListGroupItem } from 'reactstrap';
-import { PhoSizeOptions, SubOptions, ItemMain, ItemDetails } from './MenuItem.css'
-import { uuid } from 'uuidv4';
+import React from "react"
+import { Icon } from 'semantic-ui-react'
+import { ListGroupItem } from "reactstrap"
+import {
+  PhoSizeOptions,
+  SubOptions,
+  ItemMain,
+  ItemDetails,
+} from "./MenuItem.css"
+import { uuid } from "uuidv4"
 
 // TOD0: Refactor
 const MenuItem = ({ item, header }) => {
-	const { code, name, price, desc, subOptions } = item;
+  const { code, name, price, desc, subOptions } = item
 
-	// Only A5 and Pho items have supoptions
-	const getAllSubOptions = (allOptions) => {
-		return allOptions.map((option) => {
-			return <li key={uuid()}>{option}</li>;
-		});
-	};
+  // Only A5 and Pho items have supoptions
+  const getAllSubOptions = allOptions => {
+    return allOptions.map(option => {
+      return <li key={uuid()}>{option}</li>
+    })
+  }
 
-	return (
-		<ListGroupItem key={code} id="item-container">
-			<ItemMain>
-				<section className="item-name-and-code" style={{paddingRight: '1em', maxWidth: '60%'}}>
-					{code}. {name}
-				</section>
-				{header !== 'Pho' && <section className="price" style={{alignSelf: 'center'}}>{(price / 100).toFixed(2)}<button>-</button><label id={code} for="quantity"><input name={name} id={code} min='0'></input></label><button>+</button></section>}
-				{header === 'Pho' &&
-				subOptions && (
-					<PhoSizeOptions>
-							{getAllSubOptions(subOptions)}
-					</PhoSizeOptions>
-				)}
-			</ItemMain>
-			<ItemDetails>
-				{desc && <article className="description">{desc}</article>}
-				{subOptions &&
-				header !== 'Pho' && (
-					<SubOptions>
-						{getAllSubOptions(subOptions)}
-					</SubOptions>
-				)}
-			</ItemDetails>
-		</ListGroupItem>
-	);
-};
+  return (
+    <ListGroupItem key={code} id="item-container">
+      <ItemMain>
+        <section
+          className="item-name-and-code"
+          style={{ paddingRight: "1em", maxWidth: "60%" }}
+        >
+          {code}. {name}
+        </section>
+        {header !== "Pho" && (
+          <section className="price" style={{ alignSelf: "center" }}>
+            {(price / 100).toFixed(2)}
+            <label id={code} for="quantity">
+						
+							<Icon as={Icon} name='cart plus icon' bordered inverted color="teal" size='large' />
 
-export default MenuItem;
+				
+            </label>
+          </section>
+        )}
+        {header === "Pho" && subOptions && (
+          <PhoSizeOptions>{getAllSubOptions(subOptions)}</PhoSizeOptions>
+        )}
+      </ItemMain>
+      <ItemDetails>
+        {desc && <article className="description">{desc}</article>}
+        {subOptions && header !== "Pho" && (
+          <SubOptions>{getAllSubOptions(subOptions)}</SubOptions>
+        )}
+      </ItemDetails>
+    </ListGroupItem>
+  )
+}
+
+export default MenuItem
