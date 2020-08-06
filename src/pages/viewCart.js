9c901a67cae363f05736728dc362f17ea2d764e3
haphamo <hapham0392@gmail.com>
2020-08-06 11:31:32 -0400

    Removed added code, was working on a modal view but will work on that in a later feature

--- src/pages/viewCart.js
+++ src/pages/viewCart.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import SEO from '../components/seo';
-import NavDesktop from '../components/NavDesktop/NavDesktop.js';
-import Banner from '../components/Banner/Banner.js';
-import Checkout from '../components/Checkout/Checkout';
+import React from "react"
+import SEO from "../components/seo"
+import NavDesktop from "../components/NavDesktop/NavDesktop.js"
+import Banner from "../components/Banner/Banner.js"
+import Checkout from "../components/Checkout/Checkout"
 
-const ViewCart = (props) => {
-	return (
-		<section>
-			<SEO title="View Cart" />
-			<NavDesktop />
-			<Banner activeItem="View Cart" />
-			<Checkout />
-		</section>
-	);
-};
-export default ViewCart;
+const ViewCart = props => {
+  return (
+    <section>
+      <SEO title="View Cart" />
+      <NavDesktop />
+      <Banner activeItem="View Cart" />
+      <Checkout />
+    </section>
+  )
+}
+export default ViewCart
